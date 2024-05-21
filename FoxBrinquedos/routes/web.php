c0d1f547a773f841c60c6ea6f1e95ef2de3bec4c
Ggabrielasantos <119715154+Ggabrielasantos@users.noreply.github.com>
2024-05-21 19:38:04 -0300

    tela de finalizar, controller, rota de finalizar

--- FoxBrinquedos/routes/web.php
+++ FoxBrinquedos/routes/web.php
@@ -4,16 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CadastroController;
 use App\Http\Controllers\CarrinhoController;
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\FinalizarController;
 
-
-Route::get ('index', [IndexController::class, 'index'])->name('index');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get ('/', [IndexController::class, 'index'])->name('index');
 
 
 
@@ -25,9 +22,10 @@ Route::get('/produto', [ProdutoController::class,'produto'])->name('produto.inde
 
 Route::get('/produto/{produto}',[ProdutoController::class,'show'])-> name('produto.show');
 
+ Route::get('/categoria/{categoria_nome}', [ProdutoController::class, 'categoria'])->name('categoria');
 
+Route::get('finalizar', [FinalizarController::class,'finalizar'])->name('finalizar.index');
 
-Route::get ('carrinho', [CarrinhoController::class, 'carrinho'])->name('carrinho.index');
 
 
 Route::get('/dashboard', function () {
@@ -42,7 +40,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+
+    Route::get('/carrinho', [CarrinhoController::class, 'carrinho'])->name('carrinho.index');
+    Route::post('/carrinho/adicionar', [CarrinhoController::class, 'adicionar'])->name('carrinho.adicionar');
+    Route::post('/carrinho/remover/{produtoId}', [CarrinhoController::class, 'remover'])->name('carrinho.remover');
 });
 
 require __DIR__.'/auth.php';
